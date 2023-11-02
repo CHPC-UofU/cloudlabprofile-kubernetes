f10999d3e8cca8ba0cff859ede42c6fc7a6b0a71
Adam H Griffith <adam.h.griffith@utah.edu>
2023-11-02 14:04:41 -0600

    More code work in profile.py.

--- profile.py
+++ profile.py
@@ -12,52 +12,52 @@ import geni.rspec.igext as igext
 OS_IMAGE = 'urn:publicid:IDN+emulab.net+image+emulab-ops:ROCKY9-64-STD'
 
 # Create a portal context, needed to define parameters
-pc = portal.Context()
+context = portal.Context()
 
 # Create a Request object to start building RSpec
-request = pc.makeRequestRSpec()
+request = context.makeRequestRSpec()
 
 # Create some user-configurable parameters
-pc.defineParameter('node_count',
-                   'The number of nodes to allocate',
-                   portal.ParameterType.INTEGER,
-                   2)
-pc.defineParameter('public_ip_count',
-                   'The number of additional public IPs to allocate',
-                   portal.ParameterType.INTEGER,
-                   2)
+context.defineParameter('node_count',
+                        'The number of nodes to allocate',
+                        portal.ParameterType.INTEGER,
+                        2)
+context.defineParameter('public_ip_count',
+                        'The number of additional public IPs to allocate',
+                        portal.ParameterType.INTEGER,
+                        2)
 
-params = pc.bindParameters()
+params = context.bindParameters()
 
 # Validate parameters:
 if params.node_count < 2:
-    pc.reportError(portal.ParameterError('You must allocate at least 1 additional node.', ['node_count']))
+    context.reportError(
+        portal.ParameterError('You must allocate at least 1 additional node.',
+                                              ['node_count']))
 if params.public_ip_count < 2:
-    pc.reportError(portal.ParameterError('You must allocate at least 1 additional public ip.', ['public_ip_count']))
-pc.verifyParameters()
+    context.reportError(
+        portal.ParameterError('You must allocate at least 1 additional public ip.',
+                              ['public_ip_count']))
+context.verifyParameters()
 
 # Create 1 GB/s LAN:
-lan = pg.LAN()
-lan.bandwidth = 1000000 # This is in kbps.
-
-# Set up node names:
-aliases = []
-for i in range(params.node_count):
-    aliases.append("vm%02d" % i)
+lan = request.LAN()
+lan.bandwidth = 1000000  # This is in kbps.
 
 # Set up the nodes:
 ipv4_last_octet = 1
 for i in range(params.node_count):
-    node = request.XenVM(aliases[i])
-    node.disk_image = OS_IMAGE
-    request.addResource(node)
-
     ipv4_addr = "10.10.1." + str(ipv4_last_octet)
     ipv4_last_octet += 1
 
+    node = request.XenVM("vm%02d" % i)
+    node.disk_image = OS_IMAGE
+
     iface = node.addInterface("eth1")
     iface.addAddress(pg.IPv4Address(ipv4_addr, "255.255.255.0"))
+
     lan.addInterface(iface)
+    request.addResource(node)
 
 # Add LAN to request:
 request.addResource(lan)
@@ -67,4 +67,4 @@ addressPool = igext.AddressPool('MetalLBPool', int(params.public_ip_count))
 request.addResource(addressPool)
 
 # Output RSpec
-pc.printRequestRSpec(request)
+context.printRequestRSpec(request)
